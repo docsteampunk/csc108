79a5f4dff68483f5f06eb28f639adcfc1bd4c5d5
docsteampunk <larmore.daniel@gmail.com>
2021-07-06 08:49:56 -0600

    Added second sout.

--- Git-Test/src/main/java/MainGitTest.java
+++ Git-Test/src/main/java/MainGitTest.java
@@ -4,8 +4,8 @@ public class MainGitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		int num1 = 0
-		int num2 = 1
-		int answer = num1 - num2
+		int num1 = 0;
+		int num2 = 1;
+		int answer = num1 - num2;
 	}
 }

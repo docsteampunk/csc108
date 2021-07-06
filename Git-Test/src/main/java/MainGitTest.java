18ef50619a5c07bae8996f083bd284210523a5cf
docsteampunk <larmore.daniel@gmail.com>
2021-07-06 08:48:02 -0600

    Added Main File

---
+++ Git-Test/src/main/java/MainGitTest.java
@@ -0,0 +1,11 @@
+
+public class MainGitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		int num1 = 0
+		int num2 = 1
+		int answer = num1 - num2
+	}
+}
